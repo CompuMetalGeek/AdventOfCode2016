2857ef83906eaba52b22df38bc893cd35cfb9ba6
Jarno De Meyer <jarno.de.meyer@gmail.com>
2016-12-09 21:06:46 +0100

    Solved Day 9 part 2 and refactored part 1

--- src/days/Day09.java
+++ src/days/Day09.java
@@ -1,15 +1,11 @@
 package days;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Day09 extends ADay {
 
-    private boolean insideMarker, insideCompressed;
-    private int numberOfCharacters, numberOfCopies;
+    private boolean insideCompressed;
+    private int numberOfCharacters, numberOfCopies, i;
     StringBuilder sb;
 
     public Day09() {
@@ -19,58 +15,88 @@ public class Day09 extends ADay {
 
     @Override
     public void runDay() {
-        try (Scanner sc = getInput("")) {
-            while (sc.hasNext()) {
-                String next = sc.next();
-                switch (next) {
-                    case "(":
-                        processOpenBracket(sc);
-                        break;
-                    default:
-                        if (next.matches("[a-zA-Z0-9\\(\\)]+")) {
-                            sb.append(next);
-                        }
-                }
-            }
-
-            System.out.printf("%d characters in %s\n", sb.length(), sb.toString());
+        try (Scanner sc = getInput()) {
+            String input = sc.nextLine();
+            part1(input);
+            part2(input);
         }
     }
 
     private void init() {
         sb = new StringBuilder();
         insideCompressed = false;
-        insideMarker = false;
         numberOfCharacters = 0;
         numberOfCopies = 0;
+        i = 0;
     }
 
-    private void processOpenBracket(Scanner sc) {
+    private String part1(String input) {
+        init();
+        while (i < input.length()) {
+            char next = input.charAt(i);
+            i++;
+            switch (next) {
+                case '(':
+                    processOpenBracket(input);
+                    break;
+                default:
+                    if ((next + "").matches("[a-zA-Z0-9\\(\\)]+")) {
+                        sb.append(next);
+                    }
+            }
+        }
+        System.out.printf("Decompressed size with first method is %d.\n", sb.length()); // 152851
+        return sb.toString();
+    }
+
+    private void processOpenBracket(String input) {
         if (insideCompressed) {
             sb.append('(');
         } else {
-            getMarkerData(sc);
-            String toCopy = parseData(sc);
-            for (int i = 0; i < numberOfCopies; i++) {
+            getMarkerData(input);
+            String toCopy = parseData(input);
+            for (int j = 0; j < numberOfCopies; j++) {
                 sb.append(toCopy);
             }
         }
     }
 
-    private void getMarkerData(Scanner sc) {
-        Pattern p = Pattern.compile("\\d*x\\d*\\)");
-        String[] markerData = sc.findInLine(p).split("x|\\)");
+    private void getMarkerData(String input) {
+        String[] markerData = input.substring(i, input.indexOf(')', i)).split("x|\\)");
+        i += input.indexOf(')', i) - i + 1;
         numberOfCharacters = Integer.parseInt(markerData[0]);
         numberOfCopies = Integer.parseInt(markerData[1]);
 
     }
 
-    private String parseData(Scanner sc) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < numberOfCharacters; i++) {
-            sb.append(sc.next());
+    private String parseData(String input) {
+        StringBuilder sbData = new StringBuilder();
+        for (int j = 0; j < numberOfCharacters; j++) {
+            sbData.append(input.charAt(i++));
         }
-        return sb.toString();
+        return sbData.toString();
     }
 
+    private void part2(String input) {
+        long count = getCount(input);
+        System.out.printf("Decompressed size with second method is %d.\n", count); // 152851
+
+    }
+
+    private long getCount(String input) {
+        long count = 0;
+        int indexOfBracket = input.indexOf('(');
+        if (indexOfBracket == -1) {
+            return input.length();
+        }
+        String[] marker = input.substring(indexOfBracket + 1, input.indexOf(')')).split("x");
+        if (indexOfBracket != 0) {
+            count += getCount(input.substring(0, indexOfBracket));
+        }
+        count += Integer.parseInt(marker[1]) * getCount(input.substring(input.indexOf(')') + 1, input.indexOf(')') + 1 + Integer.parseInt(marker[0])));
+        if (input.indexOf(')') + 1 + Integer.parseInt(marker[0]) < input.length()) {
+            count += getCount(input.substring(input.indexOf(')') + 1 + Integer.parseInt(marker[0])));
+        }
+        return count;
+    }
 }
