23d4849162f353aa05ddc4c21bcc07937a0e6569
Jarno De Meyer <jarno.de.meyer@gmail.com>
2016-12-12 16:43:08 +0100

    Added correct loading of input file and display method

--- src/adventofcode/days/Day11.java
+++ src/adventofcode/days/Day11.java
@@ -1,18 +1,64 @@
 package adventofcode.days;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class Day11 extends ADay {
+
+    Map<String, Integer> indexOf;
+    int[] generatorLocations;
+    int[] chipLocations;
+    int elevatorLocation;
 
     public Day11() {
         super(11);
     }
 
+    public void init(int capacity) {
+        indexOf = new HashMap<>();
+        generatorLocations = new int[capacity];
+        chipLocations = new int[capacity];
+        elevatorLocation = 1;
+    }
+
     @Override
     public void runDay() {
         try (Scanner sc = getInput()) {
+            init(5);
+            int freeIndex = 0;
+            int level = 1;
+            while (sc.hasNextLine()) {
+
+                String[] line = sc.nextLine().split(" and a |^The .* contains a |, (and )*a |\\.");
+                for (int i = 1; i < line.length; i++) {
+                    String element = line[i].substring(0, 2);
+                    // check if element has been assigned index
+                    if (!indexOf.containsKey(element)) {
+                        indexOf.put(element, freeIndex);
+                        freeIndex++;
+                    }
+                    // store the current level of the object
+                    if (line[i].endsWith("generator")) {
+                        generatorLocations[indexOf.get(element)] = level;
+                    } else {
+                        chipLocations[indexOf.get(element)] = level;
+                    }
+                }
+                level++;
+            }
+            printOverview();
         }
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    private void printOverview() {
+        for (int i = 4; i >0; i--) {
+            System.out.printf("F%d  %s  ", i, elevatorLocation == i ? "E" : ".");
+            for (Entry<String, Integer> entry : indexOf.entrySet()) {
+                System.out.printf("%s  %s  ", generatorLocations[entry.getValue()] == i ? entry.getKey()+"G" : " . ", chipLocations[entry.getValue()] == i ? entry.getKey()+"M" : " . ");
+            }
+            System.out.println("");
+        }
+    }
 }
