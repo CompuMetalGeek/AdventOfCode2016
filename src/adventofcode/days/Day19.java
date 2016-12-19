1c9ec846e4187a9c4425a45ce00dc9a185b086db
Jarno De Meyer <jarno.de.meyer@gmail.com>
2016-12-19 12:14:48 +0100

    Solved Day 19

--- src/adventofcode/days/Day19.java
+++ src/adventofcode/days/Day19.java
@@ -11,8 +11,46 @@ public class Day19 extends ADay {
     @Override
     public void runDay() {
         try (Scanner sc = getInput()) {
+            while (sc.hasNext()) {
+                int number = sc.nextInt();
+
+                int winner = calculateWinner(number);
+                System.out.printf("With %d elves, elf %d gets all the presents.\n", number, winner);
+
+                int winner2 = calculateWinner2(number);
+                System.out.printf("With %d elves and the new system, elf %d gets all the presents.\n", number, winner2);
+            }
         }
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private int calculateWinner(int number) {
+        int powerOfTwo = 1;
+        while (powerOfTwo <= number) {
+            powerOfTwo *= 2;
+        }
+        powerOfTwo /= 2;
+        return (number - powerOfTwo) * 2 + 1;
+    }
+
+    private int calculateWinner2(int number) {
+        int number2 = 1;
+        int winner2 = 1;
+        boolean goDouble = false;
+        while (number2 < number) {
+            if (number2 == winner2) {
+                winner2 = 1;
+                goDouble = false;
+            } else if (winner2 == number2 * 1.0 / 2) {
+                goDouble = true;
+                winner2 += 2;
+            } else if (goDouble) {
+                winner2 += 2;
+            } else {
+                winner2++;
+            }
+            number2++;
+        }
+        return winner2;
     }
 
 }
